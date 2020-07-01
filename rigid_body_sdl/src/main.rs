6f638c2f09bce2d0145f710fdeeed0b481770c0b
Bobby <bobby@00384fe957d394corl-2.local>
2020-07-01 15:44:05 +0100

    Added loading message to sdl main.rs.

--- rigid_body_sdl/src/main.rs
+++ rigid_body_sdl/src/main.rs
@@ -6,7 +6,9 @@ use rigid_body_sdl::{
 
 fn main() -> StrResult<()> {
     let mut rigid_body_simulation = RigidBodySimulationSDL::new((800, 600))?;
+    println!("Loading simulation...");
     config::default(8, &mut rigid_body_simulation)?;
+    println!("Done.");
     while rigid_body_simulation.tick()?{}
     Ok(())
 }
